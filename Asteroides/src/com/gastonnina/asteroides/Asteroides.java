c45a381e25c394cf07e07baf8526b362f7727b08
Gaston Nina <gastonnina@gmail.com>
2013-03-24 22:37:52 -0400

    Creando metodo de acerca de

--- Asteroides/src/com/gastonnina/asteroides/Asteroides.java
+++ Asteroides/src/com/gastonnina/asteroides/Asteroides.java
@@ -1,15 +1,21 @@
 package com.gastonnina.asteroides;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Asteroides extends Activity {
-
+	
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
     }
 
     @Override
@@ -19,4 +25,8 @@ public class Asteroides extends Activity {
         return true;
     }
     
+    public void lanzarAcercaDe(View view){
+        Intent i = new Intent(this,AcercaDe.class);
+        startActivity(i);
+    }
 }

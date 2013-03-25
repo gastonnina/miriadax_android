952b74a4bbf6560ad5a4c7a8ea7eeaddaef25d1a
Gaston Nina <gastonnina@gmail.com>
2013-03-24 22:38:44 -0400

    Creando metodo de acerca de por EVENTO

--- Asteroides/src/com/gastonnina/asteroides/Asteroides.java
+++ Asteroides/src/com/gastonnina/asteroides/Asteroides.java
@@ -9,19 +9,20 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class Asteroides extends Activity {
-	private Button bAcercaDe;
-	
+
+    private Button bAcercaDe;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        bAcercaDe =(Button) findViewById(R.id.Button03);
+
+        bAcercaDe = (Button) findViewById(R.id.Button03);
         bAcercaDe.setOnClickListener(new OnClickListener() {
-        	public void onClick(View view) {
-    			lanzarAcercaDe(null);
-        	}
-       });
+            public void onClick(View view) {
+                lanzarAcercaDe(null);
+            }
+        });
     }
 
     @Override
@@ -30,9 +31,9 @@ public class Asteroides extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
-    public void lanzarAcercaDe(View view){
-        Intent i = new Intent(this,AcercaDe.class);
+
+    public void lanzarAcercaDe(View view) {
+        Intent i = new Intent(this, AcercaDe.class);
         startActivity(i);
     }
 }

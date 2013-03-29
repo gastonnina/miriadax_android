1295ea09ac0978301b5783bd43bf06b88642b5e0
Gaston Nina <gastonnina@gmail.com>
2013-03-29 12:08:54 -0400

    Detectar una pulsación sobre un elemento de la lista (completado)
    Solo tiene un toast

--- Asteroides/src/com/gastonnina/asteroides/Puntuaciones.java
+++ Asteroides/src/com/gastonnina/asteroides/Puntuaciones.java
@@ -2,7 +2,9 @@ package com.gastonnina.asteroides;
 
 import android.app.ListActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class Puntuaciones extends ListActivity {
 
@@ -22,5 +24,10 @@ public class Puntuaciones extends ListActivity {
                 android.R.layout.simple_list_item_1,
                 Asteroides.almacen.listaPuntuaciones(10)));
         /**/
+    }
+    @Override protected void onListItemClick(ListView listView, View view, int position, long id) {
+    	super.onListItemClick(listView, view, position, id);
+    	Object o = getListAdapter().getItem(position);
+    	Toast.makeText(this, "Selección: " + Integer.toString(position)+  " - " + o.toString(),Toast.LENGTH_LONG).show();
     }
 }

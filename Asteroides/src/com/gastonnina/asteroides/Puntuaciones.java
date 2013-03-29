1d9977accf62f64f4a1b59f98727ecf5ce1ae9c4
Gaston Nina <gastonnina@gmail.com>
2013-03-29 11:57:49 -0400

    -     Ejercicio: Un ListView con nuestro propio adaptador tambien corregi layout de preferencias y ountuaciones para que no sean dialog

--- Asteroides/src/com/gastonnina/asteroides/Puntuaciones.java
+++ Asteroides/src/com/gastonnina/asteroides/Puntuaciones.java
@@ -9,10 +9,14 @@ public class Puntuaciones extends ListActivity {
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.puntuaciones);
+        setListAdapter(new MiAdaptador(this,
+                Asteroides.almacen.listaPuntuaciones(10)));
+        /**
         setListAdapter(new ArrayAdapter<String>(this,
                    	R.layout.elemento_lista,
                    	R.id.titulo,
                    	Asteroides.almacen.listaPuntuaciones(10)));
+        /**/
         /**
         setListAdapter(new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1,
